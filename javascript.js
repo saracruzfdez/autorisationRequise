3433b1e33cba986df89d7344a035e9952d695674
Sara <sara@MacBook-Air-de-Clement-2.local>
2022-06-23 15:33:26 +0200

    Modifications niveau2

--- javascript.js
+++ javascript.js
@@ -1,12 +1,18 @@
-function check(){
-    var age;
-    age = Number(document.getElementById("age").value);
-    if(age < 18)
-    {
-       alert("Accès non autorisé, désolé !");
-    }
-    else
-    {
-       alert("Accès autorisé !");
-    }
+var minAge = 18;
+function _calcAge() {
+var date = new Date(document.getElementById("date").value);
+var today = new Date();
+var timeDiff = Math.abs(today.getTime() - date.getTime());
+var age1 = Math.ceil(timeDiff / (1000 * 3600 * 24)) / 365;
+return age1;
+}
+//Compares calculated age with minimum age and acts according to rules//
+function _setAge() {
+var age = _calcAge();
+//alert("my age is " + age);
+if (age < minAge) {
+alert("Désolés :( tu ne peux pas encore participer à l'élection de Jean Patate !");
+} else
+alert("Bienvenu/e à l'élection de Jean Patate 2022 !");
+window.open(main.htm, _self);
 }
